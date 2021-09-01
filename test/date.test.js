9b3d38295aeaa897b7f1591721ddcd513a161d31
Yaiser <yaiserar@gmail.com>
2021-09-01 18:21:11 +0200

    exercises structure from assembler school

---
+++ test/date.test.js
@@ -0,0 +1,16 @@
+const date = require("../src/date");
+
+// Date tests
+
+test("gets current day", () => {
+    // TODO implement
+    // expect(date.getCurrentDay()).toBe(new Date().getDay());
+});
+
+test("gets current month", () => {
+    // TODO implement
+});
+
+test("gets current year", () => {
+    // TODO implement
+});
